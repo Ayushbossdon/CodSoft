d0f7c62df45bdc2148a109caf9eef8f763413254
Ayush <ayushnagesh7@gmail.com>
2024-09-28 10:00:48 +0530

    first commit

---
+++ Portfolio/app.js
@@ -0,0 +1,11 @@
+
+/* Created by Tivotal */
+
+let typed = new Typed(".type-text", {
+    strings: ["Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+  });
+  
